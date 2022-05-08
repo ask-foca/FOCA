c03b41594b917e1eb93ebae15af1e0d20ba7da4c
summydev <adegbitesumayah@gmail.com>
2022-05-08 12:33:05 +0100

    content-changes

--- lib/screens/verify_email_screen.dart
+++ lib/screens/verify_email_screen.dart
@@ -7,34 +7,51 @@ class VerifyEmailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: <Widget>[
-          Image.asset('assets/images/send-email.png'),
-          SizedBox(
-            height: 10.0,
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset('assets/images/send-email.png'),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                'Verify Your Email',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Text(
+                'Hi Evans! Use the link below to\nto verify your email to start learning.',
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: Material(
+                  borderRadius: BorderRadius.circular(30.0),
+                  color: Color(0XFF89CFF0),
+                  child: MaterialButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Verify Email',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    minWidth: 230.0,
+                    height: 40.0,
+                  ),
+                ),
+              ),
+            ],
           ),
-          Text(
-            'Verify Your Email',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-            ),
-          ),
-          SizedBox(
-            height: 30.0,
-          ),
-          Text(
-            'Hi Evans! Use the link below to \n to verify your email to start learning.',
-          ),
-          FlatButton(
-            onPressed: () {},
-            color: Color(0XFF89cff0),
-            child: Text(
-              'Verify Emai',
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
