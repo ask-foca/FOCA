18bd4f61a36b47a37cca8d79ad1286789f97e69d
summydev <adegbitesumayah@gmail.com>
2022-05-08 11:57:52 +0100

    content-changes

--- lib/screens/verify_email_screen.dart
+++ lib/screens/verify_email_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 
 class VerifyEmailScreen extends StatelessWidget {
@@ -9,7 +11,7 @@ class VerifyEmailScreen extends StatelessWidget {
         children: <Widget>[
           Image.asset('assets/images/send-email.png'),
           SizedBox(
-            height: 5.0,
+            height: 10.0,
           ),
           Text(
             'Verify Your Email',
@@ -17,6 +19,19 @@ class VerifyEmailScreen extends StatelessWidget {
               fontSize: 20.0,
               fontWeight: FontWeight.bold,
               color: Colors.black,
+            ),
+          ),
+          SizedBox(
+            height: 30.0,
+          ),
+          Text(
+            'Hi Evans! Use the link below to \n to verify your email to start learning.',
+          ),
+          FlatButton(
+            onPressed: () {},
+            color: Color(0XFF89cff0),
+            child: Text(
+              'Verify Emai',
             ),
           ),
         ],
